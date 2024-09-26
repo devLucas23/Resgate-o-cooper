e314b0a9f300f9fe2dca4f10f8293f1e19092058
LeonidasMesquita <112254118+LeonidasQM9@users.noreply.github.com>
2024-09-26 13:23:35 -0300

    Update script.js
    Adicionando música a cena

--- script.js
+++ script.js
@@ -78,6 +78,21 @@ spaceshipMtlLoader.load('neghvar.mtl', (materials) => {
         scene.add(spaceship);
     });
 });
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+const sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load( 'music/Música Tema - Interestelar .mp3', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop( true );
+	sound.setVolume( 0.5 );
+	sound.play();
+});
 
 // Controle da nave
 window.addEventListener('keydown', (event) => {
